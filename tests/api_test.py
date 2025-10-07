961517eba81ee63c805d5924caaa3104663c15a5
Moge800 <benom0210@gmail.com>
2025-10-08 00:40:39 +0900

    feat: NG画像を表示する機能を追加

--- tests/api_test.py
+++ tests/api_test.py
@@ -45,6 +45,7 @@ def main():
     tmp = []
     ok_count = 0
     ng_count = 0
+    ng_list = []
 
     for i, img_path in enumerate(img_list):
         start = time.perf_counter()
@@ -54,6 +55,9 @@ def main():
         api_start = time.perf_counter()
         response = client.predict(img)
         api_end = time.perf_counter()
+
+        if response["label"] != "OK":
+            ng_list.append(response["image_id"]["overlay"])
 
         # 画像取得時間を測定
         img_start = time.perf_counter()
@@ -121,6 +125,18 @@ def main():
     print(f"最小処理時間: {np.min(tmp):.4f}秒")
     print(f"処理時間の標準偏差: {np.std(tmp):.4f}秒")
     print(f"詳細: {np.round(tmp, 4).tolist()}")
+
+    if len(ng_list) > 0:
+        for i, ng in enumerate(ng_list):
+            img = client.get_image(ng)
+            if img is not None:
+                cv2.putText(img, f"NG[{i}/{len(ng_list)}]", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 2)
+                cv2.putText(
+                    img, "Press any key to continue...", (10, 370), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1
+                )
+                img = cv2.resize(img, [400, 400])
+                cv2.imshow("NG image", img)
+                cv2.waitKey(0)
 
     cv2.destroyAllWindows()
 
